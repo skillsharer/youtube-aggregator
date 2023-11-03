7b5826b37374ba3d0a06a52531d32944bd1aeee5
skillsharer <skillsharer@protonmail.com>
2023-11-03 11:28:29 +0100

    Updated functions

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import os
+import logging
 from youtube_transcript_api import YouTubeTranscriptApi
 
 OPENAI_BEARER_TOKEN = os.environ['OPENAI_BEARER_TOKEN']
@@ -26,34 +27,38 @@ def chunk_words(string, chunk_size):
 def summarize(text, model_input_size = 4000):
     temp_prompt = "Please summarize the video transcript chunk focusing on the key points: "
     answer = text
+    logging.info(f'The answer length: {len(answer)}')
     while len(answer) > model_input_size:
-        answers = []
+        chunk_answers = []
         chunks = chunk_words(answer, model_input_size - len(temp_prompt))
         for chunk in chunks:
             chunk = temp_prompt + chunk
-            answer = get_text(chunk)
-            answers.append(answer)
+            chunk_response = get_text(chunk)
+            chunk_answers.append(chunk_response)
             time.sleep(3.01)
-        answer = ' '.join(answers)
+        answer = ' '.join(chunk_answers)
 
     final_prompt = f'Please summarize the video transcript focusing on the key points: {answer}'
     answer = get_text(final_prompt)
 
     return answer
 
-def get_text(prompt, model="gpt-3.5-turbo-16k"):
+def get_text(prompt, model="gpt-3.5-turbo"):
     try:
         data = {
             "model": model,
-            "messages": [
-            {"role": "user", "content": prompt}
-            ]
+            "messages": [{"role": "user", "content": prompt}]
         }
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + str(OPENAI_BEARER_TOKEN)}        
-        response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
-        response = json.loads(response.text)["choices"][0]["message"]["content"].replace('\n', '')
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + OPENAI_BEARER_TOKEN
+        }
+        response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=data)
+        response = response.json()["choices"][0]["message"]["content"].replace('\n', '')
+        logging.info(f"Response: {response}")
     except Exception as e:
         response = ""
+        logging.error(f"An error occurred: {e}")
 
     return response
 
@@ -61,4 +66,4 @@ video_id = "lchWriufJD8"
 srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
 all_text = ' '.join([element["text"] for element in srt])
 
-print(summarize(all_text, 15000))
+print(summarize(all_text, 4000))
